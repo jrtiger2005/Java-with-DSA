ca4867682d5cb4f08c50e63871660ec2746c794c
jrtiger2005 <rajputaman2005@gmail.com>
2026-08-18 17:15:18 +0530

    Implement array reversal logic in reverse.java

--- java Array/reverse.java
+++ java Array/reverse.java
@@ -12,6 +12,27 @@ public class reverse {
         }
 
         // main Logic
+
+         int i = 0;
+        int j = n-1;
+
+        while(i<j){
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+
+
+        }
+            
+        for(int k = 0; k < n; k++){
+            System.out.print(arr[k] + " ");
+        }
+        
+        
         
     }
 }
